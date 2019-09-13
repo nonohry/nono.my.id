e90e0e80cc264f6cf586f933b714dfa468c721e7
Nono Heryana <nonoheryana@gmail.com>
2019-09-13 10:38:25 +0700

    Commit Pertama

---
+++ update.sh
@@ -0,0 +1,7 @@
+git init
+git add *
+git status
+git config --global user.email "nonoheryana@gmail.com"
+git config --global user.name "Nono Heryana"
+git commit -m "Commit Pertama"
+git push origin master
